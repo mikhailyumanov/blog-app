e31a39a7d75cf5d5ae18bc5d7eedb991e34274e5
Mikhail Yumanov <yumanov_m@mail.ru>
2019-02-07 00:20:30 +0300

    Fourth commit

--- wall/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class PostForm(forms.Form):
-    forms.CharField()
-    your_name = forms.CharField(max_length=100)
-    post_text = forms.CharField(widget=forms.Textarea, max_length=5000)
